f49b2b7f00dcc9be6e3a8f2c2dd487d92702627e
Sasha Zakablukov <Zakablukov777@gmail.com>
2018-05-17 10:13:29 +0300

    complete gind.js

--- src/bind.js
+++ src/bind.js
@@ -1,3 +1,26 @@
 global.bind = function (fn, context) {
-
+	 var argArray = [].slice.call(arguments, 2);
+    return function(){
+        return fn.apply(context, argArray)
+   }
 };
+
+var user = {
+    name : "sasha"
+};
+
+var user2 = {
+    name : "vova"
+};
+
+var printName = function (age, height){
+    return this.name + age + height;
+};
+
+var newPrintName = myBind(printName, user2, 15, 20);
+
+
+console.log(printName.call(user));
+console.log(printName.call(user2));
+console.log(newPrintName.call(user));
+console.log(newPrintName.call(user2));
