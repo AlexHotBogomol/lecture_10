64a190fc2c2dd83016cf53320dc6c816e3bff7a1
Sasha Zakablukov <Zakablukov777@gmail.com>
2018-05-17 11:36:37 +0300

    all done

--- src/bind.js
+++ src/bind.js
@@ -1,20 +1,20 @@
 global.bind = function (fn, context) {
-	var argArray = [].slice.call(arguments, 2);
-    return function(){
-        return fn.apply(context, argArray)
-   }
+  var argArray = [].slice.call(arguments, 2);
+  return function() {
+    return fn.apply(context, argArray);
+  };
 };
 
 var user = {
-    name : "sasha"
+  name: 'sasha'
 };
 
 var user2 = {
-    name : "vova"
+  name: 'vova'
 };
 
-var printName = function (age, height){
-    return this.name + age + height;
+var printName = function (age, height) {
+  return this.name + age + height;
 };
 
 var newPrintName = global.bind(printName, user2, 15, 20);
--- src/measuredExecutionTime.js
+++ src/measuredExecutionTime.js
@@ -1,3 +1,16 @@
 global.measuredExecutionTime = function (fn) {
-
+  return function() {
+    var result;
+    console.time('execution time');
+    result = fn.apply(this, arguments);
+    console.timeEnd('execution time');
+    return result;
+  };
 };
+
+var sum = function(a, b) {
+  return a + b;
+};
+
+var measuredSum = global.measuredExecutionTime(sum);
+console.log(measuredSum(10, 20));
